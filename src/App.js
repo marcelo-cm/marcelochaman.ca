d36eafbdcd3e2a60aa9fc28e29081afab22fcc7d
Marcelo ChamanMallqui <marcechaman@gmail.com>
2023-05-03 18:26:05 -0400

    shipping cool feature on website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home.jsx";
+import Inspect from "inspx";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Inspect>
+        <Home />
+      </Inspect>
     </div>
   );
 }
